ace812cfdb755b1b8422c2181f13e82fdee0b924
ngpong <pengpeng1997428@gmail.com>
2025-10-30 15:53:36 +0800

    ...

--- algorithm/sort/test.cpp
+++ algorithm/sort/test.cpp
@@ -1,44 +1,48 @@
 #include "common.hpp"
 
-void solution_quicksort_02(Array &arr, int32_t n) {
-  auto partition = [&](int32_t low, int32_t high) -> int32_t {
-    int32_t pivot = arr[low];
-    int32_t lt = low, gt = high;
-    while (lt < gt) {
-      while (lt < gt && arr[gt] >= pivot) {
-        gt--;
-      }
-      while (lt < gt && arr[lt] <= pivot) {
-        lt++;
-      }
-      if (lt < gt) {
-        std::swap(arr[lt], arr[gt]);
-      }
-    }
-    std::swap(arr[lt], arr[low]);
-
-    return lt;
+void solution_radix_sort(Array &arr, int32_t n) {
+  auto digit = [](int32_t num, int32_t exp) -> int32_t {
+    return (num / exp) % 10;
   };
 
-  std::stack<std::tuple<int32_t, int32_t>> s;
-  s.push({ 0, n - 1 });
-  while (!s.empty()) {
-    auto [low, high] = s.top(); s.pop();
-    if (low >= high) {
-      continue;
+  auto radix_count = [&](int32_t exp) -> void {
+    Array sorter(n, 0x0);
+    Array bucket(10, 0x0);
+
+    for (int32_t i = 0; i < n; i++) {
+      bucket[digit(arr[i], exp)]++;
     }
 
-    int32_t mid = partition(low, high);
-    s.push({ mid + 1, high });
-    s.push({ low, mid - 1});
+    for (int32_t i = 1; i < 10; i++) {
+      bucket[i] += bucket[i - 1];
+    }
+
+    for (int32_t i = n - 1; i >= 0; i--) {
+      sorter[--bucket[digit(arr[i], exp)]] = arr[i];
+    }
+
+    arr.swap(sorter);
+  };
+
+  int32_t max = arr[0];
+  for (int32_t i = 1; i < n; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+
+  for (int32_t exp = 1; max / exp > 0; exp *= 10) {
+    radix_count(exp);
   }
 }
 
 int32_t main(void) {
   std::vector<int32_t> arr = {
-    9, 8, 7, 6, 5, 5, 5, 5, 4, 3, 2, 1
+    1, 15, 9, 12, 7, 3, 5, 4, 6, 21
   };
-  solution_quicksort_02(arr, arr.size());
+  solution_radix_sort(arr, arr.size());
+
+  std::cout << arr << std::endl;
 
   return EXIT_SUCCESS;
 }
